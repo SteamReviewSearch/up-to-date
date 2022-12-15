b269fea9e7308cf9ca66b1fa73b734e08e89473f
Minjae Kim <x.xreesespuffs@gmail.com>
2022-12-16 00:59:43 +0900

    final

--- weekPlay.js
+++ weekPlay.js
@@ -1,27 +1,35 @@
-const mainThread=require('./new_game/index_game')
+const mainThread = require("./new_game/index_game");
 function setTimeoutPromise(ms) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => resolve(), ms);
-    });
-  }
-const schedule=async()=>{
-    let plan=new Date().toISOString();
-    do {
-        const now = new Date();
-        let year = now.getFullYear(); // 년
-        let month = now.getMonth();   // 월
-        let day = now.getDate();      // 일
-        let hours = now.getHours(); // 시
-        let minutes = now.getMinutes();  // 분
-        let seconds = now.getSeconds();  // 초
-        let today= new Date(year, month, day,hours,minutes,seconds).toISOString();
-        if(today >= plan){
-            await new mainThread().index_game()
-            plan=new Date(year, month, day+7).toISOString();
-            console.log(plan)
-        }
-        console.log("now: ",today)
-        await setTimeoutPromise(360000);
-    } while (1);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(), ms);
+  });
 }
+const schedule = async () => {
+  let plan = new Date().toISOString();
+  do {
+    const now = new Date();
+    let year = now.getFullYear(); // 년
+    let month = now.getMonth(); // 월
+    let day = now.getDate(); // 일
+    let hours = now.getHours(); // 시
+    let minutes = now.getMinutes(); // 분
+    let seconds = now.getSeconds(); // 초
+    let today = new Date(
+      year,
+      month,
+      day,
+      hours,
+      minutes,
+      seconds
+    ).toISOString();
+    if (today >= plan) {
+      plan = new Date(year, month, day + 7).toISOString();
+      await new mainThread().index_game();
+      await require("./ES_data_update/redis.js");
+      await require("./ES_data_update/redis_appid.js");
+    }
+    console.log("now: ", today);
+    await setTimeoutPromise(360000);
+  } while (1);
+};
 schedule();
